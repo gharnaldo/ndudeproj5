fee8094237f448a6dbf025c4dc80ccb2c915e5cf
Arnaldo <inboxarnaldo@gmail.com>
2023-02-15 15:39:54 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Project: Data Pipelines
+
+## Project Description
+
+This Project will allow Sparkify Startup analyze the data they've been collecting on songs and user activity on their new music streaming app. They will be able to move their processes and data onto the cloud. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+
+
+## Files in repository
+
+1. **dag.py** Dag.
+2. **sql_statements.py** contains the queries used by dag.py.
+
+
---
+++ sql_statements.py
@@ -0,0 +1,86 @@
+STAGING_EVENTS_SQL = """
+copy {} from '{}' 
+ACCESS_KEY_ID '{{}}'
+SECRET_ACCESS_KEY '{{}}'
+region 'us-west-2' format as JSON '{}'
+"""
+
+STAGING_SONGS_SQL = """
+copy {} from '{}' 
+ACCESS_KEY_ID '{{}}'
+SECRET_ACCESS_KEY '{{}}'
+region 'us-west-2' format as JSON 'auto'
+"""
+
+COPY_STAGING_EVENTS_SQL = STAGING_EVENTS_SQL.format(
+    "staging_events",
+    "s3://udacity-dend/log_data",
+    "s3://udacity-dend/log_json_path.json"
+)
+
+COPY_STAGING_SONGS_SQL = STAGING_SONGS_SQL.format(
+    "staging_songs",
+    "s3://udacity-dend/song_data/A/A/A/TRAAAAK128F9318786.json"
+)
+
+SONGPLAY_TABLE_LOAD =  """
+    INSERT INTO songsplay (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent) SELECT
+        TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second'),
+        e.userid,
+        e.level,
+        s.song_id,
+        s.artist_id,
+        e.sessionid,
+        e.location,
+        e.useragent
+    FROM staging_events e
+    LEFT JOIN staging_songs s ON
+        e.song = s.title AND
+        e.artist = s.artist_name AND
+        ABS(e.length - s.duration) < 2
+    WHERE
+        e.page = 'NextSong'
+"""
+
+USER_TABLE_LOAD = """
+INSERT INTO users
+    SELECT
+        distinct e.userid,e.firstname,e.lastname,e.gender,e.level
+    FROM 
+        staging_events e
+    WHERE
+        e.page = 'NextSong'
+"""
+                     
+SONG_TABLE_LOAD = """
+INSERT INTO songs
+    SELECT
+        distinct s.song_id, s.title, s.artist_id, s.year, s.duration
+    FROM 
+        staging_songs s
+"""
+                     
+ARTIST_TABLE_LOAD = """
+INSERT into artists
+    SELECT
+        distinct s.artist_id,s.artist_name,s.artist_location,
+        s.artist_latitude,s.artist_longitude
+    FROM 
+        staging_songs s
+"""
+                       
+TIME_TABLE_LOAD = """
+INSERT into times
+    SELECT
+        TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second'),
+        extract(hour from TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second')) as hour,
+        extract(day from TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second')) as day,
+        extract(week from TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second')) as week,
+        extract(month from TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second')) as month,
+        extract(year from TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second')) as year,
+        extract(weekday from TIMESTAMP 'epoch' + (e.ts/1000 * INTERVAL '1 second')) as weekday       
+    FROM 
+        staging_events e
+    WHERE
+        e.page = 'NextSong'
+"""
